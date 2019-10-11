c319ed878003754eb938b8be2253f33947f821ef
Alexis <reche06@gmail.com>
2019-10-10 21:10:15 -0400

    This time I actually forked the project

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit Binding - occurs when dot notation is used to invoke a function
+* 2. New Binding- occurs when the new keyword is used to create a new obj. the new object is "this".
+* 3. Explicit Binding- looks at where the function is being invoked. Can use method such as .call .apply .bind to assign "this".
+* 4. Global Binding - "this" is the Window/Console Object.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,48 @@
 // Principle 1
 
 // code example for Window Binding
-
-// Principle 2
-
-// code example for Implicit Binding
-
-// Principle 3
-
-// code example for New Binding
-
-// Principle 4
-
-// code example for Explicit Binding
+function saySomething() {
+    console.log(this)
+  }
+  
+  // or
+  
+  setTimeout( function() {console.log(this)}, 1000)
+  
+  // Principle 2
+  
+  // code example for Implicit Binding
+  const MyObject = function () {
+    this.name = 'MyObjectName';
+    this.attribute = 'attribute';
+  };
+  
+  MyObject.prototype.doSomething = function (something) {
+    console.log(this.name + ' is ' + something + '!');
+  }
+  
+  const obj = new MyObject();
+  
+  obj.doSomething('epic');
+  
+  // Principle 3
+  
+  // code example for New Binding
+  
+  function RunningPerson(runner) {
+    this.pace = 'is running a 6 min/mile.';
+    this.runner = runner;
+    this.speak = function() {
+        console.log(this.runner + this.pace);
+        console.log(this);
+    }
+  }
+  
+  const john = new RunningPerson('John');
+  john.speak();
+  
+  // Principle 4
+  
+  // code example for Explicit Binding
+  const coder = {name: 'John', myFavoriteActivity: 'coding'};
+  MyObject.prototype.doSomething.call(coder, coder.myFavoriteActivity);
